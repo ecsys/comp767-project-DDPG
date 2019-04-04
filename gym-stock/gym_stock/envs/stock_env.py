3d2754b77c91695de92c4493a40e2385975a49e2
Ecsys <18.yishi@gmail.com>
2019-04-04 02:52:25 -0400

    reset and init done

--- gym-stock/gym_stock/envs/stock_env.py
+++ gym-stock/gym_stock/envs/stock_env.py
@@ -7,30 +7,52 @@ from gym.utils import seeding
 
 class StockEnv(gym.Env):
 
-    # constant
-    DOW_JONES = ["AXP","AAPL","BA","CAT","CSCO",
-             "CVX","DWDP","XOM","GS","HD",
-             "IBM","INTC","JNJ","KO","JPM",
-             "MCD","MMM","MRK","MSFT","NKE",
-             "PFE","PG","TRV","UNH","UTX",
-             "VZ","V","WBA","WMT","DIS"]
+    def __init__(self, tickers, train_start_date, start_balance):
 
-    def __init__(self, n_stock, train_start, train_end, test_start, test_end):
+
+        # constant
+        tickers = ["AXP","AAPL","BA","CAT","CSCO",
+                "CVX","DWDP","XOM","GS","HD",
+                "IBM","INTC","JNJ","KO","JPM",
+                "MCD","MMM","MRK","MSFT","NKE",
+                "PFE","PG","TRV","UNH","UTX",
+                "VZ","V","WBA","WMT","DIS"]
         
-        self.state = {'price': np.zeros(n_stock),
-                      'holding': np.zeros(n_stock),
+        data_dir = ''
+
+        self.train_start_date = train_start_date
+        self.n_stock = len(tickers)
+        self.tickers = tickers
+        self.start_balance = start_balance
+
+        # read all data into memory when initializing
+        self.stock_data = self.load_data(data_dir, tickers)
+        
+        # initialize state, action, and date indices
+        self.state = {'price': np.zeros(self.n_stock),
+                      'holding': np.zeros(self.n_stock),
                       'balance': 0}
-
-        self.action = np.zeros(n_stock)
-
-        self.current_day = {}
+        self.action = np.zeros(self.n_stock)
+        self.current_day = [] 
+        self.reset()
 
     def step(self, action):
         pass
         # def get_reward(self, cur_state, action, next_state):
 
     def reset(self):
-        pass
+        
+        self.current_day = self.get_index_from_date(self.train_start_date)
+
+        prices = []
+        for idx, ticker in enumerate(self.tickers):
+            ticker_row_idx = self.current_day[idx]
+            prices.append(self.stock_data[ticker].iloc[ticker_row_idx]['open'])
+        self.state['price'] = np.array(prices)
+        self.state['holding'] = np.zeros(self.n_stock)
+        self.state['balance'] = self.start_balance
+
+        self.action = np.zeros(self.n_stock)
 
     def render(self):
         pass
@@ -45,6 +67,38 @@ class StockEnv(gym.Env):
 
     def load_next_day_state(self):
         pass
+
+    def get_index_from_date(self, date):
+        stock_date_index = []
+        for ticker in self.tickers:
+            stock_df = self.stock_data[ticker]
+            stock_date_index.append(stock_df[stock_df['timestamp']==date].index.values.astype(int)[0])
+        return stock_date_index
+
+    def is_valid_action(self, action):
+        valid_action = True
+        amount_required = 0
+        for i in range(len(self.tickers)):
+            # cannot sell or buy partial stock
+            if not isinstance(action[i], np.int64):
+                valid_action = False
+                break
+            # cannot sell more than you have, no short operation allowed
+            if not self.state['holding'][i] >= action[i]:
+                valid_action = False
+                break
+            if action[i] < 0:
+                amount_required += abs(action[i]) * self.state['price'][i]
+        # cannot spend more money than you have to buy stocks
+        if amount_required > self.state['balance']:
+            valid_action = False
+
+        return valid_action
+        
+        
+
+        
+
     
     
 
