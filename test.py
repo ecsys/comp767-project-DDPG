9580fab4e1718b6ec2f745b140704cabb1d7d3e7
grant81 <grantzhao75@gmail.com>
2019-04-07 15:48:30 -0400

    updated done condition

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-import gym
 from DDPG import DDPG
 from generateNoise import OUNoise
-import gym_stock
 from hyperparameters import *
 from stock_env import *
 env = StockEnv()
@@ -11,7 +9,8 @@ noise = OUNoise(env.action_space)
 for eposide in range(100):
     state = env.reset()
     episode_reward = 0
-    for step in range(500):
+    done = False
+    while not done:
         action = agent.sample_action(state)
         action = noise.get_action(action=action)
         new_state, reward, done = env.step(action)
