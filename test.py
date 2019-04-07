079962c83c72b34b168de91f2311fb4d531650f1
grant81 <grantzhao75@gmail.com>
2019-04-07 16:49:05 -0400

    update logging

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from DDPG import DDPG
 from generateNoise import OUNoise
 from hyperparameters import *
 from stock_env import *
+
 env = StockEnv()
 rewards = []
 agent = DDPG(env)
@@ -10,20 +11,20 @@ for eposide in range(100):
     state = env.reset()
     episode_reward = 0
     done = False
+    step_num = 0
     while not done:
+        step_num += 1
         action = agent.sample_action(state)
         action = noise.get_action(action=action)
         new_state, reward, done = env.step(action)
-        agent.memory.push(state,action,reward,new_state,done)
+        agent.memory.push(state, action, reward, new_state, done)
         if agent.memory.check_full():
             agent.update(BATCH_SIZE)
         state = new_state
-        episode_reward+= reward
+        episode_reward += reward
+        if step_num % 500 == 0:
+            print('eposide: {}, step: {}, reward: {}'.format(eposide, step_num, reward))
         if done:
-            print('eposide: {}, reward: {}'.format(eposide,reward))
+            print('eposide: {}, reward: {}'.format(eposide, reward))
             break
     rewards.append(episode_reward)
-
-
-
-
